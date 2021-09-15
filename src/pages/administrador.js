b6061ac6aaa4e69cacd02f97ae175e2eb6186411
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-09-15 17:55:04 -0500

    adminsitrador

---
+++ src/pages/administrador.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import reportGenerator from "../../src/templates/services/reportGenerator";
+
+// markup
+const Administrador = () => {
+  return (
+    <>
+      <h1>hola</h1>
+      <reportGenerator />
+    </>
+  );
+};
+
+export default Administrador;
