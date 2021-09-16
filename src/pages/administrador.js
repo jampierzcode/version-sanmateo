3e8d8b010bfea1334e6f7131985f4734713385f1
hectoremilio1000 <hectoremilio1000@gmail.com>
2021-09-16 08:05:31 -0500

    hola admin

--- src/pages/administrador.js
+++ src/pages/administrador.js
@@ -1,12 +1,10 @@
 import * as React from "react";
-import reportGenerator from "../../src/templates/services/reportGenerator";
 
 // markup
 const Administrador = () => {
   return (
     <>
       <h1>hola</h1>
-      <reportGenerator />
     </>
   );
 };
